878a29fcfb90b6e031dd6cfe2455a5df949bf69a
Trisha Chavan <trisha.chavan20@gmail.com>
2026-03-31 13:42:01 +0530

    file uploaded

---
+++ DS 9.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+ // Node structure
+struct Node {
+ int data;
+ Node* next;
+};
+Node* head = NULL;
+// Insert at end
+void insert(int value) {
+ Node* newNode = new Node();
+ newNode->data = value;
+ newNode->next = NULL;
+ if (head == NULL) {
+ head = newNode;
+ } else {
+ Node* temp = head;
+ while (temp->next != NULL) {
+ temp = temp->next;
+ }
+ temp->next = newNode;
+ }
+}
+// Display linked list
+void display() {
+ Node* temp = head;
+ if (temp == NULL) {
+ cout << "List is empty";
+ return;
+ }
+ while (temp != NULL) {
+ cout << temp->data << " -> ";
+ temp = temp->next;
+ }
+ cout << "NULL";
+}
+int main() {
+ insert(10);
+ insert(20);
+ insert(30);
+ cout << "Linked List: ";
+ display();
+ return 0;
+}
